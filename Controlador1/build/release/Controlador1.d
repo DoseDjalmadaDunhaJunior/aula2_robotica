011ba38f41225afeeed4599c623693d0fdb9f40d
DoseDjalmadaDunhaJunior <djalminhajr_cunha@hotmail.com>
2020-05-14 13:57:43 -0300

    ainda não esta funcionando adequadamente

---
+++ Controlador1/build/release/Controlador1.d
@@ -0,0 +1,6 @@
+build/release/Controlador1.o: Controlador1.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/motor.h
---
+++ Controlador2/build/release/Controlador2.d
@@ -0,0 +1,6 @@
+build/release/Controlador2.o: Controlador2.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/motor.h
